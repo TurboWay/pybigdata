55b1bb6a07ce541ca13d2c41d4a05b2af7d6a0c9
way <way>
2021-03-30 15:53:04 +0800

    kafka producer 均匀写到所有分区

--- kafka/demo_producer.py
+++ kafka/demo_producer.py
@@ -7,6 +7,7 @@
 
 import json
 from kafka import KafkaProducer
+from random import choice
 
 servers = ['172.16.122.17:9092', ]
 producer = KafkaProducer(bootstrap_servers=servers,
@@ -15,4 +16,6 @@ producer = KafkaProducer(bootstrap_servers=servers,
 topic = 'test'
 key = 'hi'
 value = 'way'
-producer.send(topic=topic, key=key, value=value).get(timeout=10)
+partitions = producer.partitions_for(topic) # 获取所有分区，均匀地写到分区中
+for i in range(10000):
+    producer.send(topic=topic, partition=choice(list(partitions)), key=key, value=value).get(timeout=10)
