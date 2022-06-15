9ee1e83f04cfa73d7e67ad3cbb56e188871081ff
way <way>
2022-06-15 15:54:51 +0800

    update

--- doris/streamload.py
+++ doris/streamload.py
@@ -5,73 +5,113 @@
 # @Site :
 # @Describe: doris 数据导入
 
-import time
+import logging
 import base64
 import json
+import time
 from requests import Session
 
-"""
-支持两种格式数据导入，json/csv(默认\t \n)
-curl -u user:pwd -T test.csv --retry-delay 1 --retry 20 --location-trusted http://127.0.0.1:8030/api/database/table/_stream_load
-"""
+
+def Logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s [%(name)s] %(levelname)s: %(message)s', "%Y-%m-%d %H:%M:%S")
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
+
+
+logger = Logger(__name__)
+
+
+# 重试装饰器
+def retry(func):
+    max_retry = 3
+
+    def run(*args, **kwargs):
+        for i in range(max_retry + 1):
+            if i > 0:
+                logger.warning(f"等待3秒后，尝试第{i}次重试...")
+            time.sleep(3)
+            flag = func(*args, **kwargs)
+            if flag:
+                return flag
+
+    return run
 
 
 class DorisSession:
 
-    def __init__(self, host, port, database, table, user, pwd):
-        self.Authorization = base64.b64encode((user + ':' + pwd).encode('utf-8')).decode('utf-8')
-        self.url = f'http://{host}:{port}/api/{database}/{table}/_stream_load'
-        self.table = table
+    def __init__(self, fe_servers, database, user, passwd, sequence_col=None):
+        """
+        :param fe_servers: fe servers ['127.0.0.1:8030', '127.0.0.2:8030', '127.0.0.3:8030']
+        :param database: 数据库名称
+        :param user: 用户名
+        :param passwd: 密码
+        :param sequence_col: sequence_col 名称, 默认不启用
+        """
+        self.fe_servers = fe_servers
+        self.database = database
+        self.Authorization = base64.b64encode((user + ':' + passwd).encode('utf-8')).decode('utf-8')
         self.sesson = Session()
         self.sesson.headers = {
             'Expect': '100-continue',
             'Authorization': 'Basic ' + self.Authorization,
             'format': 'json',
-            'strip_outer_array': 'true', # json数组
+            'strip_outer_array': 'true',  # json数组
             'fuzzy_parse': 'true',  # 数组中每一行的字段顺序完全一致,加速导入速度
-            'function_column.sequence_col': 'source_sequence',  # 保证数据顺序, 需启用 Sequence Column 功能, 详见 http://palo.baidu.com/docs/操作手册/数据更新与删除/Sequence-Column
         }
+        if sequence_col:  # 保证数据顺序, 需启用 Sequence Column 功能
+            self.sesson.headers['function_column.sequence_col'] = sequence_col
 
-    @property
-    def label(self):
-        return f"{self.table}_{time.strftime('%Y%m%d_%H%M%S', time.localtime())}"
+    def _label(self, table):
+        return f"{table}_{time.strftime('%Y%m%d_%H%M%S', time.localtime())}"
 
-    def get_be(self):
-        response = self.sesson.put(self.url, '', allow_redirects=False)
-        if response.status_code == 307:
-            return response.headers['Location']
+    def _columns(self, keys):
+        return ','.join([f'`{column}`' for column in keys])
 
-    def send(self, data):
-        url = self.get_be()  # doris fe 转发有bug，需要处理307
-        self.sesson.headers['label'] = self.label
-        self.sesson.headers['columns'] = ','.join(data[0].keys())
+    def _get_be(self, table):
+        for fe_server in self.fe_servers:
+            host, port = fe_server.split(':')
+            url = f'http://{host}:{port}/api/{self.database}/{table}/_stream_load'
+            response = self.sesson.put(url, '', allow_redirects=False)
+            if response.status_code == 307:
+                return response.headers['Location']
+        else:
+            raise Exception(f"执行失败，获取不到可用的BE节点，请检查 fe servers 配置")
+
+    @retry
+    def insertdb(self, table, data):
+        url = self._get_be(table)
+        self.sesson.headers['label'] = self._label(table)
+        self.sesson.headers['columns'] = self._columns(data[0].keys())
         response = self.sesson.put(url, json.dumps(data), allow_redirects=False)
         if response.status_code == 200:
             res = response.json()
             if res.get('Status') == 'Success':
-                print(res)
+                logger.info(res)
                 return True
             else:
-                print(f"fail: {res}")
+                logger.error(res)
                 return False
         else:
-            print(f"error: {response.text}")
+            logger.error(response.text)
             return False
 
 
 if __name__ == "__main__":
-    doris = DorisSession(
-        host='',
-        port=8030,
-        database='',
-        table='test',
-        user='',
-        pwd=''
-    )
+    doris_cfg = {
+        'fe_servers': ['127.0.0.1:8030', '127.0.0.2:8030', '127.0.0.3:8030'],
+        'database': '',
+        'user': '',
+        'passwd': '',
+    }
+    doris = DorisSession(**doris_cfg)
     json_data = [
-        {'dateid': '20211014', 'shop_code': 'sdd', 'sale_amount': '100', 'source_sequence':14},
+        {'dateid': '20211014', 'shop_code': 'sdd', 'sale_amount': '100', 'source_sequence': 14},
         {'dateid': '20211014', 'shop_code': 'sdd', 'sale_amount': '4', 'source_sequence': 5},
         {'dateid': '20211014', 'shop_code': 'sdd', 'sale_amount': '3', 'source_sequence': 2},
     ]
-    doris.send(json_data)
-
+    doris.insertdb('test', json_data)
