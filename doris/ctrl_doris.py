5320247a66368d24b1e3d1bdd9b7ec51d11f339c
Way <1143496751@qq.com>
2022-10-29 16:19:04 +0800

    Update ctrl_doris.py

--- doris/ctrl_doris.py
+++ doris/ctrl_doris.py
@@ -5,9 +5,9 @@
 # @Site :
 # @Describe: doris 数据导入
 
-from DorisClient import DorisSession, logger as DorisLogger
+from DorisClient import DorisSession, DorisLogger, Logger
 
-# DorisLogger.setLevel('ERROR')  # 日志输出，默认INFO级别
+# DorisLogger.setLevel('ERROR')  # default:INFO
 
 doris_cfg = {
     'fe_servers': ['10.211.7.131:8030', '10.211.7.132:8030', '10.211.7.133:8030'],
@@ -16,9 +16,8 @@ doris_cfg = {
     'passwd': '123456',
 }
 doris = DorisSession(**doris_cfg)
-# doris.conn.close()  # 默认会创建一个连接，用于执行sql，不需要的话，可以关闭
 
-# 一般导入
+# append
 data = [
     {'id': '1', 'shop_code': 'sdd1', 'sale_amount': '99'},
     {'id': '2', 'shop_code': 'sdd2', 'sale_amount': '5'},
@@ -26,13 +25,13 @@ data = [
 ]
 doris.streamload(table='streamload_test', json_array=data)
 
-# 删除导入
+# delete
 data = [
     {'id': '1'},
 ]
 doris.streamload(table='streamload_test', json_array=data, merge_type='DELETE')
 
-# 删写混合导入
+# merge
 data = [
     {'id': '10', 'shop_code': 'sdd1', 'sale_amount': '99', 'delete_flag': 0},
     {'id': '2', 'shop_code': 'sdd2', 'sale_amount': '5', 'delete_flag': 1},
@@ -40,7 +39,7 @@ data = [
 ]
 doris.streamload(table='streamload_test', json_array=data, merge_type='MERGE', delete='delete_flag=1')
 
-# Sequence 一般导入
+# Sequence append
 data = [
     {'id': '1', 'shop_code': 'sdd1', 'sale_amount': '99', 'source_sequence': 11, },
     {'id': '1', 'shop_code': 'sdd2', 'sale_amount': '5', 'source_sequence': 2},
@@ -48,7 +47,7 @@ data = [
 ]
 doris.streamload(table='streamload_test', json_array=data, sequence_col='source_sequence')
 
-## Sequence + 删写混合导入
+## Sequence merge
 data = [
     {'id': '1', 'shop_code': 'sdd1', 'sale_amount': '99', 'source_sequence': 100, 'delete_flag': 0},
     {'id': '1', 'shop_code': 'sdd2', 'sale_amount': '5', 'source_sequence': 120, 'delete_flag': 0},
@@ -56,10 +55,3 @@ data = [
 ]
 doris.streamload(table='streamload_test', json_array=data, sequence_col='source_sequence', merge_type='MERGE',
                  delete='delete_flag=1')
-
-# 执行 sql 有返回值, 比如 select
-rows = doris.read('select * from streamload_test limit 1')
-print(rows)
-
-# 执行 sql 无返回值, 比如 ddl
-doris.execute('truncate table streamload_test')
