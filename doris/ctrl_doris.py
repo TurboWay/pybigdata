fa42b9c81c5da0726c28c3df3c5e95eb59d5279d
caiwei <caiwei@anta.com>
2023-02-17 12:10:58 +0800

    update

--- doris/ctrl_doris.py
+++ doris/ctrl_doris.py
@@ -23,13 +23,13 @@ data = [
     {'id': '2', 'shop_code': 'sdd2', 'sale_amount': '5'},
     {'id': '3', 'shop_code': 'sdd3', 'sale_amount': '3'},
 ]
-doris.streamload(table='streamload_test', json_array=data)
+doris.streamload('streamload_test', data)
 
 # delete
 data = [
     {'id': '1'},
 ]
-doris.streamload(table='streamload_test', json_array=data, merge_type='DELETE')
+doris.streamload('streamload_test', data, merge_type='DELETE')
 
 # merge
 data = [
@@ -37,7 +37,7 @@ data = [
     {'id': '2', 'shop_code': 'sdd2', 'sale_amount': '5', 'delete_flag': 1},
     {'id': '3', 'shop_code': 'sdd3', 'sale_amount': '3', 'delete_flag': 1},
 ]
-doris.streamload(table='streamload_test', json_array=data, merge_type='MERGE', delete='delete_flag=1')
+doris.streamload('streamload_test', data, merge_type='MERGE', delete='delete_flag=1')
 
 # Sequence append
 data = [
@@ -45,13 +45,35 @@ data = [
     {'id': '1', 'shop_code': 'sdd2', 'sale_amount': '5', 'source_sequence': 2},
     {'id': '2', 'shop_code': 'sdd3', 'sale_amount': '3', 'source_sequence': 1},
 ]
-doris.streamload(table='streamload_test', json_array=data, sequence_col='source_sequence')
+doris.streamload('streamload_test', data, sequence_col='source_sequence')
 
-## Sequence merge
+# Sequence merge
 data = [
     {'id': '1', 'shop_code': 'sdd1', 'sale_amount': '99', 'source_sequence': 100, 'delete_flag': 0},
     {'id': '1', 'shop_code': 'sdd2', 'sale_amount': '5', 'source_sequence': 120, 'delete_flag': 0},
     {'id': '2', 'shop_code': 'sdd3', 'sale_amount': '3', 'source_sequence': 100, 'delete_flag': 1},
 ]
-doris.streamload(table='streamload_test', json_array=data, sequence_col='source_sequence', merge_type='MERGE',
+doris.streamload('streamload_test', data, sequence_col='source_sequence', merge_type='MERGE',
                  delete='delete_flag=1')
+
+
+# streamload default retry config:  max_retry=3, retry_diff_seconds=3
+# if you don't want to retry, "_streamload" can help you
+doris._streamload('streamload_test', data)
+
+# if you want to changed retry config, follow code will work
+from DorisClient import DorisSession, Retry
+
+max_retry = 5
+retry_diff_seconds = 10
+
+
+class MyDoris(DorisSession):
+
+    @Retry(max_retry=max_retry, retry_diff_seconds=retry_diff_seconds)
+    def streamload(self, table, dict_array, **kwargs):
+        return self._streamload(table, dict_array, **kwargs)
+
+
+doris = MyDoris(**doris_cfg)
+doris.streamload('streamload_test', data)
